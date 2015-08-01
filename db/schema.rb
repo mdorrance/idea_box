fcdafec5933326c81b0c6b2d866af12d6cf85f0f
Michael Dorrance <dorrancemd@gmail.com>
2015-08-01 15:08:23 -0600

    adds redirect to ideas

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801193113) do
+ActiveRecord::Schema.define(version: 20150801203736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "ideas", ["category_id"], name: "index_ideas_on_category_id", using: :btree
+  add_index "ideas", ["user_id"], name: "index_ideas_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -23,4 +41,6 @@ ActiveRecord::Schema.define(version: 20150801193113) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "users"
 end
--- spec/features/user_creation_spec.rb
+++ spec/features/user_creation_spec.rb
@@ -5,5 +5,15 @@ feature "User creates an account" do
     visit new_user_path
     expect(page).to have_button('Join Idea Box')
   end
+  scenario "user submits valid form" do
+    visit new_user_path
+    fill_in "Username", with: "miked"
+    fill_in "Password", with: "1234"
+    click_button "Join Idea Box"
+
+    current_path.should == ideas_path
+
+
+  end
 
 end
